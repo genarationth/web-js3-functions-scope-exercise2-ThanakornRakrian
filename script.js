7a0cc6660ac9d5ae408aa6221f640515f16dd5ba
thanakorn <thanakornrakrian@gmail.com>
2024-05-14 15:39:34 +0700

    js3 exercise2 functions scope finish

---
+++ script.js
@@ -0,0 +1,8 @@
+// const evenNumber = function(number){
+//     number%2 == 1 ? console.log('even number') : console.log('odd number');
+// };
+
+const evenNumber = (number) =>
+  number % 2 == 1 ? console.log("even number") : console.log("odd number");
+
+evenNumber(6);
